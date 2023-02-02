2b996f350d8ea2e7c4c1acb135de0c68b3781a70
furlong123 <120377091+furlong123@users.noreply.github.com>
2023-02-02 11:09:22 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Yes)
+        basic.pause(2000)
+        basic.showIcon(IconNames.No)
+        basic.pause(2000)
+        led.stopAnimation()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    Kitronik_Robotics_Board.allOff()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    for (let index = 0; index < 4; index++) {
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 100)
+        basic.pause(1000)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, 50)
+    }
 })
